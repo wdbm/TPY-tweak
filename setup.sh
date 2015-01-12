3fea4862b0ee1c00d4591eceb1f68ff9c077c9f3
Will Breaden Madden <zicodian@gmail.com>
2015-01-12 21:54:22 +0000

    testing

--- setup.sh
+++ setup.sh
@@ -43,10 +43,36 @@ echo "${configuration}" > ~/.bashrc
 
 # Install driver for wheel emulation scrolling.
 
-wget https://raw.githubusercontent.com/wdbm/TPY-tweak/master/xserver-xorg-input-evdev_2.8.2-1_amd64.deb
-sudo dpkg -i xserver-xorg-input-evdev_2.8.2-1_amd64.deb
-rm xserver-xorg-input-evdev_2.8.2-1_amd64.deb
+sudo apt-get build-dep xserver-xorg-input-evdev xserver-xorg-input-synaptics
+
+wget https://aur.archlinux.org/packages/xf/xf86-input-evdev-trackpoint/xf86-input-evdev-trackpoint.tar.gz
+tar -xzf xf86-input-evdev-trackpoint.tar.gz
+git clone git://git.debian.org/git/pkg-xorg/driver/xserver-xorg-input-evdev
+git clone git://git.debian.org/git/pkg-xorg/driver/xserver-xorg-input-synaptics
+
+mv xf86-input-evdev-trackpoint arch
+mv xserver-xorg-input-evdev evdev
+mv xserver-xorg-input-synaptics synaptics
+
+cp synaptics/src/{eventcomm.c,eventcomm.h,properties.c,synaptics.c,synapticsstr.h,synproto.c,synproto.h} evdev/src
+cp synaptics/include/synaptics-properties.h evdev/src
+cp arch/*.patch evdev
+
+cd evdev
+patch -p1 -i 0001-implement-trackpoint-wheel-emulation.patch
+patch -p1 -i 0008-disable-clickpad_guess_clickfingers.patch
+patch -p1 -i 0010-add-synatics-files-into-Makefile.am.patch
+
+dpkg-buildpackage
+
+cd ..
+sudo dpkg -i xserver-xorg-input-evdev_*.deb
 sudo apt-get remove xserver-xorg-input-synaptics
+
+#wget https://raw.githubusercontent.com/wdbm/TPY-tweak/master/xserver-xorg-input-evdev_2.8.2-1_amd64.deb
+#sudo dpkg -i xserver-xorg-input-evdev_2.8.2-1_amd64.deb
+#rm xserver-xorg-input-evdev_2.8.2-1_amd64.deb
+#sudo apt-get remove xserver-xorg-input-synaptics
 
 # Lock packages.
 
